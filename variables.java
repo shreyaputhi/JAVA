47fbefddc82ff35be8faab3a02cfa1b5c9eb65c2
Shreya Puthi <shreya.x.puthi.-nd@disney.com>
2024-12-09 14:48:40 +0530

    added 2 files

---
+++ variables.java
@@ -0,0 +1,17 @@
+public class variables 
+{
+    public static void main(String a[])
+    {
+        System.out.println(3+5);
+		System.out.println(8+7);
+		
+		int num1=3;
+		int num2=5;
+		System.out.println(num1+num2);
+		
+		int num11=3;
+		int num21=5;
+		int result=num11+num21;
+		System.out.println(result);
+    }
+}
